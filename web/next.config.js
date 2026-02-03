2243afef4e51ec1354362f6ea0228a053399f796
pranay5255 <pranay5255@yahoo.com>
2026-02-03 20:23:22 +0000

    Remove distDir and output settings from next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Match the Vercel Output Directory setting and avoid static export mode.
-  distDir: 'dist',
-  output: 'standalone'
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
